1b58743dade8c769a460ea68b32f0df50a58ba16
Arthur de Araújo Farias <arthur@afarias.org>
2025-11-06 15:30:56 +0100

    Current work on custom memory streaming protocol

---
+++ sources-vhdl-2000-packages/axi4s.linalg.basic.vhdl
@@ -0,0 +1,9 @@
+-- Package Declaration Section
+package custom_stream is
+   
+end package custom_stream;
+ 
+-- Package Body Section
+package body custom_stream is
+ 
+end package body custom_stream;
